0da80cbb62bb70b88dec7b9049e0ac79723fc50c
clank201 <clank201@gmail.com>
2014-09-24 19:27:32 +0200

    Adaptant el lector de mapa de string array a char matrix

--- ROGUELIKE/ROGUELIKE.cpp
+++ ROGUELIKE/ROGUELIKE.cpp
@@ -3,31 +3,7 @@
 #include <string>
 
 using namespace std;
-void txttostring(){
-	int i = 1;
-	string buffer[255];
-	ifstream textfile;
-	textfile.open("map.txt");
-	// check
-	if (textfile.fail()){
-		cout << "error al obrir";
-	}
-	while (!textfile.eof()){
-		textfile >> buffer[i];
-		cout << buffer[i] << endl;
-		i++;
-	}
-	textfile.close();
-	/*string FileMeasure = "Hello FILE!"          //Trosdecodi k xuta pero no es meu
-		int TempNumOne = FileMeasure.size();      //S ha d adaptar per una string array
-	char Filename[100];                           //Trulàs Ombrívol
-	for (int a = 0; a <= TempNumOne; a++)
-	{
-		Filename[a] = FileMeasure[a];
-	}
-	*/
 
-}
 int main()
 {
 	cout << "Launcher by..." << endl;
@@ -42,15 +18,40 @@ int main()
 		cout << "Escull una opcio" << endl << "1) Programa1" << endl << "2) Crear mapa" << endl << "3) Opcions" << endl << "4) Exit" << endl;
 		cin >> option;
 		if (option == 1){
-			txttostring();
+			int i = 0;
+			string buffer[255];
+			ifstream textfile;
+			textfile.open("map.txt");
+			if (textfile.fail()){
+				cout << "error al obrir";
+			}
+			while (!textfile.eof()){
+				textfile >> buffer[i];
+				//cout << buffer[i] << endl;
+				i++;
+			}
+			textfile.close();
+		   
+			int x = 0;
+			int y = 0;
+			int TempNumOne = buffer[y].size();       //AquestTroçDeCodiNoEsMeu    
+			char Filename[255][255];                 //
+			for (int x = 0; x <= TempNumOne; x++)    //
+			{                                        //
+			Filename[x][y] = buffer[y][x];              //APartirDAquiElCodiJaEsMeu
+			}
+			cout << Filename[0][1]<<endl;
 		}
 		if (option == 2){
-			int i = 5;
+			int i;
+			cout << "Introdueix el numero de files: ";
+			cin >> i;
 			ofstream textfile;
 			textfile.open("map.txt");
 			if (textfile.fail()){
 				cerr << "error al obrir";
 			}
+			
 			while (i != 0){
 				string temp;
 				cin >> temp;
