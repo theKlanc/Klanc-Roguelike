0b9ddf7cfcd854f0f400f39c75d4a9f353d91239
clank201 <clank201@gmail.com>
2014-09-26 19:59:40 +0200

    Començat el joc, screenbuffer i tecles implementades, pero les tecles no semblen funcionar gaire bé.

--- ROGUELIKE/ROGUELIKE.cpp
+++ ROGUELIKE/ROGUELIKE.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
@@ -18,31 +19,85 @@ int main()
 		cout << "Escull una opcio" << endl << "1) Programa1" << endl << "2) Crear mapa" << endl << "3) Opcions" << endl << "4) Exit" << endl;
 		cin >> option;
 		if (option == 1){
-			int i = 0, bufferheight,bufferlength;
-			string buffer[255];
+			/*d aqui fins al proxim comment es la carrega de mapa*/
+			int i = 0, mapheight,maplength;
+			string temp1[255];
 			ifstream textfile;
 			textfile.open("map.txt");
 			if (textfile.fail()){
 				cerr << "error al obrir";
 			}
-			textfile >> bufferheight;
-			textfile >> bufferlength;
-			cout << bufferheight << endl << bufferlength << endl;
-			bufferheight = bufferheight - 1; //apanyo
+			textfile >> mapheight;
+			textfile >> maplength;
+			cout << mapheight << endl << maplength << endl;
+			mapheight = mapheight - 1; //NOapanyo
+			maplength = maplength - 1; //otro apanyo, tot i k potser al final no son apanyosEDIT:nohoson
 			while (!textfile.eof()){
-				textfile >> buffer[i];
+				textfile >> temp1[i];
 				i++;
 			}
 			textfile.close();
-			char map[255][255];                     //
-			for (int y = 1; y <= bufferheight; y++)      //EhtoEhMio el y=1 hauria d esser 0
+			char map[255][255];                          //
+			for (int y = 0; y <= mapheight; y++)      //EhtoEhMio el y=1 hauria d esser 0
 			{
-				for (int x = 1; x <= bufferlength; x++)  //
+				for (int x = 0; x <= maplength; x++)  //
 				{                                        //
-					map[x][y] = buffer[y][x];       //APartirDAquiElCodiJaEsMeu
+					map[x][y] = temp1[y][x];            //APartirDAquiElCodiJaEsMeu
 				}
 			}
-			cout << map[0][0];
+			/*for (int i = 0; i <= mapheight; i++){
+				for (int j = 0; j <= maplength; j++){
+					cout << map[j][i];
+				}
+				cout << endl;
+			}*/
+			
+			
+			
+			
+			/*a partir d aqui comensa el joc...*/
+			
+
+			int posx = 1, posy = 1;
+			char screenbuffer[255][255];
+			while (1){
+				//int tecla = _getch();
+				int tecla = 22480;
+				switch (tecla){
+				case 22475 : {
+						posx = posx - 1;
+					}
+					case 22472 : {
+						posy = posy - 1;
+					}
+					case 22480 : {
+						posy = posy + 1;
+					}
+					case 22477 : {
+						posx = posx + 1;
+					}
+					default:{}
+				}
+				for (int i = 0; i <= mapheight; i++){
+					for (int j = 0; j <= maplength; j++){
+						screenbuffer[j][i]= map[j][i];
+					}
+				}
+				cout << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl; 
+				screenbuffer[posx][posy] = 'O';
+				for (int i = 0; i <= mapheight; i++){
+					for (int j = 0; j <= maplength; j++){
+						cout << screenbuffer[j][i];
+					}
+					cout << endl;
+				}
+			}
+
+
+
+
+
+
 		}
 		if (option == 2){
 			int i, j;
@@ -69,7 +124,10 @@ int main()
 
 		}
 		if (option == 3){
-			//generatemapOPCIONS;
+			//generatemapOPCIONS;HIJACKED TEST
+			while (1){
+				cout << _getch();
+			}
 		}
 		if (option == 4){
 			return 0;
